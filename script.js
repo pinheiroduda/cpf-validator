0a9441829276f1c19008be9407c495b42b3a23ac
Eduarda <eduardapinheiro1302@gmail.com>
2021-08-10 11:21:14 -0300

    feat: verfication of cpf validation

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function cpfValidation(cpf) {
+  return false;
+}
+
 function validation() {
-  console.log("starting cpf validation")
+  console.log("starting cpf validation");
+
+  var cpf = document.getElementById('typed_cpf').value;
+  
+  // verificando se o cpf é válido //
+  var validationResult = cpfValidation(cpf);
+  if (validationResult) {
+    document.getElementById('success').style.display = 'block';
+  } else {
+    document.getElementById('error').style.display = 'block'
+  }
 }
